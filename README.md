d246f839c4d7cee2e3892f00c10b3ed55ac88c94
Ruth Westnidge <rwestnidge@gmail.com>
2022-07-29 14:10:37 +0100

    Updates index.html style.css script.js README.md

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 The Odin Project Foundations - Project: Make An Online Calculator 
+https://www.theodinproject.com/lessons/foundations-calculator
 
-Early look at development process - 
+Other work commitments put this project on hold a number of times for many weeks, recently revisited it:
+Main branch currently contains work in progress
+Other branches show 'development process', ideas, experiments 
 
-First approach: 
-Starting the CSS & rendering the numbers & operators using DOM methods (version 1 halted, pushed here)
-
-Second approach:
-Using data attributes to define number key presses versus operator key presses (in progress)
+Currently the calculator will evaluate one sum
+Next steps are:
+- allow it to evaluate a series of sums, 
+- add in decimal point, 'clear' and 'backspace' functionality, round of decimal point numbers, 
+- debug 
+- finish the HTML and CSS including accessibility checks 
+- add in keyboard support
 
 Aim: basic maths calculator that simply does the sums you want it to. 
 Takes input by user clicking on buttons or entering numbers and operators with keyboard. 
+Outputs: to a 'calculator display panel' showing user input then the sum's total. 
 
 Skills: HTML, CSS, JavaScript 
 
--- script.js
+++ script.js
@@ -1,76 +1,112 @@
-let register0 = 0;
 let operator = '';
-let result = operate();
-let num1 = ' ';
-console.log(num1);
-let num2 = ' ';
-console.log(num2);
+let num1 = 0;
+let num2 = 0;
+let total = 0;
+let displayValue = '';
+let isOperatorPressed = false;
 
-// adds maths operations 
+const operatorKeys = document.querySelectorAll('.op_btn');
+const numberKeys = document.querySelectorAll('.num_btn');
+const clearButton = document.getElementById('btn_clr');
+const equalButton = document.getElementById('equals');
+
+
+
+// adds basic maths operations 
 const add = (num1, num2) => {
   console.log(num1);
   console.log(num2);
-  let total = num1 + num2;
+  total = num1 + num2;
   console.log(total);
   return total;
 }
 
-//add(2, 4);
-
-const subtract = (num1, num2) => {
-  let total = num1 - num2;
+const subtract = (a, b) => {
+  console.log(num1);
+  console.log(num2);
+  total = a - b;
   console.log(total);
   return total;
 }
 
-//subtract(6, 4);
-
-const multiply = (num1, num2) => {
-  let total = num1 * num2;
+const multiply = (a, b) => {
+  console.log(num1);
+  console.log(num2);
+  total = a * b;
   console.log(total);
   return total;
 }
 
-//multiply(2, 4);
-
-const divide = (num1, num2) => {
-  let total = num1 / num2;
+const divide = (a, b) => {
+  console.log(num1);
+  console.log(num2);
+  total = a / b;
   console.log(total);
   return total;
 }
 
-//divide(6, 3);
-
-function operate(operator, num1, num2) {
+// calls appropriate basic maths functions according to operator 
+function operate(num1, operator, num2) {
   if (operator === '+') {
-    return add();
+    return add(num1, num2);
   } else if (operator === '-') {
-    return subtract();
-  } else if (operator === '*') {
-    return multiply();
+    return subtract(num1, num2);
+  } else if (operator === 'X') {
+    return multiply(num1, num2);
   } else if (operator === '/') {
-    /*if (y === 0) { 
-    return 0} else { */
-    return divide();
+    if (num1 === 0) {
+      return 0
+    } else {
+      return divide(num1, num2);
+    }
   }
 }
 
-//operate(add);
 
-const displayOperator = document.querySelectorAll('.op_btn');
 
-for (let i = 0; i < displayOperator.length; i++) {
-  displayOperator.item(i).addEventListener('click', () => {
-    register0 = document.getElementById('disp').textContent;
-    // displays operator
-    document.getElementById('disp').textContent = document.getElementById('disp').textContent + displayOperator.item(i).textContent;
-  })
+//  sets behaviour onclick for any number button user clicks on 
+for (let i = 0; i < numberKeys.length; i++) {
+  numberKeys.item(i).addEventListener('click', e => updateDisplay(e));
 }
 
-const displayNumbers = document.querySelectorAll('.num_btn');
-for (let i = 0; i < displayNumbers.length; i++) {
-  displayNumbers.item(i).addEventListener('click', () => {
-    register0 = document.getElementById('disp').textContent;
-    document.getElementById('disp').textContent = document.getElementById('disp').textContent + displayNumbers.item(i).textContent;
-  })
+//  sets behaviour onclick for any operator button user clicks on 
+for (let i = 0; i < operatorKeys.length; i++) {
+  operatorKeys.item(i).addEventListener('click', e => handleOperator(e));
 }
+
+//  sets behaviour onclick when user clicks on equals button 
+equals.addEventListener('click', e => handleEquals(e));
+
+
+
+// saves numbers to global variables, renders numbers to display_container
+function updateDisplay(e) {
+  if (isOperatorPressed && document.getElementById("disp").textContent.match(/[\+|\-|\X|\/]\d*/)) {
+    document.getElementById("disp").textContent = '';
+  }
+  displayValue = document.getElementById("disp").textContent + e.target.textContent;
+  document.getElementById("disp").textContent = displayValue;
+  if (isOperatorPressed) {
+    num2 = parseFloat(displayValue);
+  } else {
+    num1 = parseFloat(displayValue);
+  }
+}
+
+//saves operator to global variables, renders operator to display_container
+function handleOperator(e) {
+  num1 = parseFloat(displayValue);
+  displayValue = e.target.textContent;
+  document.getElementById("disp").textContent = displayValue;
+  operator = e.target.textContent;
+  isOperatorPressed = true;
+}
+
+// calls operate() when equals button clicked, renders value returned to display_container 
+function handleEquals(e) {
+  document.getElementById('disp').textContent = operate(num1, operator, num2);
+}
+
+// next steps: 
+//enable a second sum to be performed 
+// complete the HTML layout, restyle the title, finish layout HTML, CSS 
